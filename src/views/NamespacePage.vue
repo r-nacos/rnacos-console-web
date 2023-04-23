1bed696f8a2ffa504e1816091bf082d7773c9af7
heqingpan <heqingpan@126.com>
2023-04-24 00:56:33 +0800

    完成新建命名空间功能

--- src/views/NamespacePage.vue
+++ src/views/NamespacePage.vue
@@ -28,12 +28,44 @@
     </div>
     </div>
     <div class="form" v-show="useForm">
-        <div>
+        <div class="clear form-top">
             <div class="form-close"> 
                 <n-icon size="20" @click="closeForm">
                     <Close/>
                 </n-icon>
             </div>
+        </div>
+        <div> 
+            <n-form :model="model" :rules="rules">
+                <n-form-item path="namespaceName" label="命名空间名称">
+                    <n-input v-model:value="model.namespaceName" @keydown.enter.prevent />
+                </n-form-item>
+                <n-form-item path="namespaceId" label="命名空间Id">
+                    <n-input v-model:value="model.namespaceId" @keydown.enter.prevent />
+                </n-form-item>
+    
+                <n-row :gutter="[0, 24]">
+                    <n-col :span="24">
+                        <div style="display: flex; justify-content: flex-end">
+                        <n-button
+                            :disabled="model.namespaceId === null"
+                            round
+                            @click="closeForm"
+                        >
+                            返回
+                        </n-button>
+                        <n-button
+                            :disabled="model.namespaceId === null"
+                            round
+                            type="primary"
+                            @click="create"
+                        >
+                            确认
+                        </n-button>
+                        </div>
+                    </n-col>
+                </n-row>
+            </n-form>
         </div>
     </div>
 </div>
@@ -85,12 +117,45 @@ export default defineComponent({
         }]);
         const loadingRef = ref(false)
         const useFormRef = ref(false)
+        const modelRef = ref({
+            'namespaceId':'',
+            'namespaceName':'',
+        })
+        const rules ={
+            namespaceId: [
+                {
+                required: true,
+                validator (rule , value ) {
+                    if (!value) {
+                    return new Error('需要输入ID')
+                    }
+                    return true
+                },
+                trigger: ['input', 'blur']
+                }
+            ],
+
+            namespaceName: [
+                {
+                required: true,
+                validator (rule , value ) {
+                    if (!value) {
+                    return new Error('需要输入名称')
+                    }
+                    return true
+                },
+                trigger: ['input', 'blur']
+                }
+            ],
+        }
         return {
             useForm:useFormRef,
             columns,
             data: dataRef,
+            model: modelRef,
             pagination: false,
             loading: loadingRef,
+            rules,
             rowKey (rowData) {
                 return rowData.namespaceId
             },
@@ -109,9 +174,32 @@ export default defineComponent({
             },
             showCreate(){
                 useFormRef.value=true;
+                modelRef.value = {
+                    'namespaceId':'',
+                    'namespaceName':'',
+                };
             },
             closeForm(){
                 useFormRef.value=false;
+            },
+            doCreate(){
+                namespaceApi.add(modelRef.value).then(res=>{
+                    if(res.status==200){
+                        if(res.data.code==200){
+                            this.closeForm();
+                            this.doLoadNamespace();
+                        }
+                        else{
+                            window.$message.error(res.data.message);
+                        }
+                    }
+                    else{
+                        window.$message.error("request err,status code:"+res.status);
+                    }
+                })
+                .catch(err=>{
+                    window.$message.error(err.message)
+                })
             }
         }
     },
@@ -120,6 +208,12 @@ export default defineComponent({
             console.log("loadNamespace")
             this.doLoadNamespace()
         },
+        create(){
+            this.doCreate()
+        },
+    },
+    created(){
+        this.loadNamespace()
     }
 })
 </script>
@@ -143,6 +237,7 @@ export default defineComponent({
 
 
 .form{
+    padding: 0 5px;
     z-index: 1;
     position:absolute;
     width:400px;
@@ -152,10 +247,16 @@ export default defineComponent({
     border-left: #ccc 1px solid;
 }
 
+.form-top {
+    clear:both;
+    height:30px;
+}
+
 .form-close{
     cursor: pointer;
     font-size:20px;
     line-height: 30px;
+    float:right;
     height: 30px;
     width: 30px;
     padding: 5px;
